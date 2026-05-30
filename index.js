51707a9618f5c1d024b29a9f0452a3e42f70bde2
Thaiarass <thaiara10@hotmail.com>
2026-05-29 21:52:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import { registerRootComponent } from 'expo';
-import App from './App';
+import {AppRegistry} from "react-native";
+import App from "./App";
+import {name as appName} from "./package.json";
 
-registerRootComponent(App);
+AppRegistry.registerComponent(appName, () => App);
